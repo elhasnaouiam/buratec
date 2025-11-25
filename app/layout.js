6d112001f4c6ccf5a25c7b24ec620b48951dfb2e
elhasnaouiam <er-rachidia@hotmail.fr>
2025-11-25 14:21:48 +0100

    Create RootLayout component with metadata

---
+++ app/layout.js
@@ -0,0 +1,9 @@
+export const metadata = { title: 'BURATEC' }
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  )
+}
